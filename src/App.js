50c5d4040ef99f7a834bb9d44da817c1437f2f4d
libertadpozos <libertad.pozos@gmail.com>
2019-06-10 23:19:26 +0200

    Merge branch checkbox

--- src/App.js
+++ src/App.js
@@ -1,142 +1,139 @@
-import React from 'react';
-import './App.scss';
-import ListCharacters from './components/ListCharacters'
-import Filters from './components/Filters'
+import React from "react";
+import "./App.scss";
+import ListCharacters from "./components/ListCharacters";
+import Filters from "./components/Filters";
 import { Switch, Route } from "react-router-dom";
-import DetailCard from './components/DetailCard';
-
-
-
+import DetailCard from "./components/DetailCard";
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    this.state={
-      data:[],
+    this.state = {
+      data: [],
       isFetching: true,
-      filters:{
-        byName:"",
-        byHouse:[]
+      filters: {
+        byName: "",
+        byHouse: []
       }
-    }
-    this.getData=this.getData.bind(this);
-    this.handleInputFilterName=this.handleInputFilterName.bind(this)
-    this.handleInputFilterHouse=this.handleInputFilterHouse.bind(this)
+    };
+    this.getData = this.getData.bind(this);
+    this.handleInputFilterName = this.handleInputFilterName.bind(this);
+    this.handleInputFilterHouse = this.handleInputFilterHouse.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getData();
   }
 
-  getData(){
-    fetch('http://hp-api.herokuapp.com/api/characters')
-    .then(response=> response.json())
-    .then(data=> {
-      const newData= data.map((character,index)=>{
-        return {
-          ...character,
-          id: index+1 
+  getData() {
+    fetch("http://hp-api.herokuapp.com/api/characters")
+      .then(response => response.json())
+      .then(data => {
+        const newData = data.map((character, index) => {
+          return {
+            ...character,
+            id: index + 1
+          };
+        });
+        this.setState({
+          data: newData,
+          isFetching: false
+        });
+      });
+  }
+
+  handleInputFilterName(event) {
+    const inputValue = event.target.value;
+    this.setState(prevState => {
+      return {
+        filters: {
+          ...prevState.filters,
+          byName: inputValue
+        }
+      };
+    });
+  }
+
+  handleInputFilterHouse(event) {
+    const inputValue = event.target.value;
+    console.log(inputValue);
+
+    this.setState(prevState => {
+      return {
+        filters: {
+          ...prevState.filters,
+          byHouse: prevState.filters.byHouse.find(house => house === inputValue)
+            ? prevState.filters.byHouse.filter(house => house !== inputValue)
+            : prevState.filters.byHouse.concat(inputValue)
+        }
+      };
+    });
+  }
+
+  getFilteredList() {
+    return this.state.data
+      .filter(character => {
+        if (
+          character.name
+            .toLowerCase()
+            .includes(this.state.filters.byName.toLowerCase())
+        ) {
+          return true;
+        } else {
+          return false;
         }
       })
-    this.setState({
-      data: newData,
-      isFetching: false
-      }
-    )
-    })
+      .filter(character =>
+        !this.state.filters.byHouse.length
+          ? true
+          : this.state.filters.byHouse.includes(character.house)
+      );
   }
 
-  handleInputFilterName(event){
-    const inputValue= event.target.value
-    this.setState(prevState=>{
-      return {  
-          filters:{
-            ...prevState.filters,
-            byName: inputValue
-          } 
-      }
-    })
-  }
-  
-  handleInputFilterHouse(event){
-    const inputValue= event.target.value
-    console.log(inputValue)
-    
-    this.setState(prevState=>{
-      return {
-        filters:{
-          ...prevState.filters,
-          byHouse: prevState.filters.byHouse.find(house=> house===inputValue)? 
-          prevState.filters.byHouse.filter(house=>house !== inputValue)
-          : prevState.filters.byHouse.concat(inputValue)
-        }
-      }
-    })
-  }
-
-  getFilteredList(){
+  render() {
+    const { data, isFetching } = this.state;
     return (
-      this.state.data
-      .filter(character => {
-      if (character.name.toLowerCase().includes(this.state.filters.byName.toLowerCase())) {
-        return true;
-      } else {
-        return false;
-      }
-    })
-    .filter(character=> 
-     !this.state.filters.byHouse.length?
-     true
-     : this.state.filters.byHouse.includes(character.house)
-    )
-    )
-  }
-  
-  render(){
-  const {data, isFetching}=this.state;
-  return (
-    <div className="general__container">
-      <header>
-      <h1 className="title">It's LeviOsa not LeviosA</h1>
-      </header>
-      {isFetching 
-      ? <p>Loading...</p>
-      : (
-        <React.Fragment>
-        <Switch>
-          <Route
-          exact path="/"
-          render={() => (
-            <main className="main__container">
-            <Filters 
-            onChangeName={this.handleInputFilterName}
-            inputValueName={this.state.filters.byName}
-            onChangeHouse={this.handleInputFilterHouse}
-            />
-            <ListCharacters data={this.getFilteredList()}/>
-          </main>
-          )}
-      />
+      <div className="general__container">
+        <header>
+          <h1 className="title">It's LeviOsa not LeviosA</h1>
+        </header>
+        {isFetching ? (
+          <p>Loading...</p>
+        ) : (
+          <React.Fragment>
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={() => (
+                  <main className="main__container">
+                    <Filters
+                      onChangeName={this.handleInputFilterName}
+                      inputValueName={this.state.filters.byName}
+                      onChangeHouse={this.handleInputFilterHouse}
+                    />
+                    <ListCharacters data={this.getFilteredList()} />
+                  </main>
+                )}
+              />
 
-      <Route
-      path="/card/:cardId"
-      render={routerProps=>(
-        <DetailCard match={routerProps.match} data={data} />
-      )}
-      />
-      </Switch>
-      </React.Fragment>
-      )
-    }
-    <footer className="footer__container">
-        <p className="title__footer">Harry Potter Cards</p>
-        <p className="author">Libertad Pozos</p>
-        <p className="year__footer">2019</p>
-      
-    </footer>
-    </div>
-  );
-}
+              <Route
+                path="/card/:cardId"
+                render={routerProps => (
+                  <DetailCard match={routerProps.match} data={data} />
+                )}
+              />
+            </Switch>
+          </React.Fragment>
+        )}
+        <footer className="footer__container">
+          <p className="title__footer">Harry Potter Cards</p>
+          <p className="author">Libertad Pozos</p>
+          <p className="year__footer">2019</p>
+        </footer>
+      </div>
+    );
+  }
 }
 export default App;
